abc393d0b09144d740d02b410ae72b03d2a0c221
kingdom_lf@163.com <kingdom_lf@163.com>
2017-11-23 17:42:43 +0800

    初始化为spring boot项目，使用maven构建

---
+++ src/main/java/com/lifeng/Java7ConcurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.lifeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java7ConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java7ConcurrencyApplication.class, args);
+	}
+}
